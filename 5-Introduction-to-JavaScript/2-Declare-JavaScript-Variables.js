247ce5286c20b76e029b16af4c615ca1a3fc8eac
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 12:58:33 -0600

    Completed lesson 5.2

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+/*LESSON SUMMARY: Learned about variables in JavaScript. Variables represent the data we want to refer to.  */
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
