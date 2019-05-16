c13ef68fd1efdb43382a88efd5ecf943362a814f
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 13:16:28 -0600

    Completed lesson 5.5

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Learned about undefined variables. Initialized varibales a, c, & c so they are not undefined. */ 
+
+// Initialize these three variables
+var a= 5;
+var b= 10;
+var c= "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
