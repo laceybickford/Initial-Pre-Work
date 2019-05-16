3ea58a42a19017dbf7887ec5c65a10e1f609c818
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 13:21:44 -0600

    Completed lesson 5.6

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Learned that in JavaScript all variables and function names are case sensitive. Modified existing declarations & assignments to use camelCase. */
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar= 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
