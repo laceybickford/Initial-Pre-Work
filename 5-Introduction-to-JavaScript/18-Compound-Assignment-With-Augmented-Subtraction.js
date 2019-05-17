8824f77839e3342948a0265fdef78727d9ee16a4
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 22:38:03 -0600

    Completed lesson 5.18

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Used the -= opeator to assign a new value to the variable by subtracting the number to the right of the operators. */
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
