e9c85eb033e5a40cabeb29517afddac2e99d094e
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-17 13:57:13 -0600

    Completed lesson 5.23

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+/*LESSON SUMMARY: You can remove escape characters if you change the first and last quotes into ' ' instead of " ".  */
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
