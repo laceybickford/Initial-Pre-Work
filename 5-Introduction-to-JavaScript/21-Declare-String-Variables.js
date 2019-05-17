75eb17cb879f9d8d76a9524b7f27fa17af83f37c
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 22:45:15 -0600

    Completed lesson 5.21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+/*LESSON SUMMARY: Created two string variables and assigned them with my first and last name.*/
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Lacey";
+var myLastName = "Bickford";
