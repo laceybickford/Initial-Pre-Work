860d446ea930022f228b2421fee75ee77866abec
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 22:40:22 -0600

    Completed lesson 5.19

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Used the *= operator to assign and multiply a variable by a number. */
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
