b6598bdea5557196de10bf0f5304a104a06059c5
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-16 22:31:32 -0600

    Completed lesson 5.17

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Learned to use the += operator to assign a new value with addition. */ 
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
