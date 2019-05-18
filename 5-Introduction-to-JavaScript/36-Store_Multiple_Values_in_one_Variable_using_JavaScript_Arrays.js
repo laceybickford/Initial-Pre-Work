7f9d45be2c56534c604143e55b2e804c7628351b
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 11:09:53 -0600

    Completed lesson 5.36

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+/*LESSON SUMMARY: learned about arrays and stored multiple values in one varible. */ 
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["string", 16];
