286e32bbc150e69aa28cc083d26e87e55f1d3fa1
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 14:45:07 -0600

    Completed lesson 5.51

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+/*LESSON SUMMARY: Created a function that accepts a value, multiplied and returned a new value.  */ 
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive (num) {
+return num * 5;
+}
+
+console.log(minusSeven(10));
