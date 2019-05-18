48505c7f72fb5a59f373fab6364dda746d323c25
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 16:00:24 -0600

    Completed lesson 5.60

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Learned about the inequality operator. */ 
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(99);
