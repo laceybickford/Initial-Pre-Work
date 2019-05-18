b13b726782b9a8569b9b86557f4c2cef9cdc5baf
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 12:00:13 -0600

    Completed lesson 5.47

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+/*LESSON SUMMARY: Passed values to functions with an argument. */ 
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs (a, b) {
+  console.log(a + b);
+}
+functionWithArgs(16, 21);
