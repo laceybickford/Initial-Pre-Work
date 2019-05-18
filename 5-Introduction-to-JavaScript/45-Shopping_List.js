faf4cc62b8f75daf52a771417273198456409a81
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 11:50:53 -0600

    Completed lesson 5.45

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,9 @@
+/*LESSON SUMMARY: Created a shopping list using the variable myList and arrays. */
+
+var myList = [
+    ["Orange Juice", 1],
+    ["eggs", 12],
+    ["apples", 6],
+    ["bananas", 8],
+    ["seltzer water", 24],
+];
