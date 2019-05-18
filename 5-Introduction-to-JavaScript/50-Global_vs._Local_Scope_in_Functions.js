c3dd262deaea8038663347821fdcf010ae93c3cf
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 12:18:46 -0600

    Completed lesson 5.50

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+/*LESSON SUMMARY: Learned about local scope. */ 
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+
+  var myVar;
+
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+//console.log(myVar);
+
+// Now remove the console log line to pass the test
