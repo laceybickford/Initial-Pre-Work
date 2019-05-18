f0dd89451f1ee87831000ce5587842e702255721
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 11:15:45 -0600

    Completed lesson 5.38

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+/*LESSON SUMMARY: Accessed array item based on index with bracket notation. */ 
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData= myArray[0]; // equals 50
+
+// Only change code below this line.
