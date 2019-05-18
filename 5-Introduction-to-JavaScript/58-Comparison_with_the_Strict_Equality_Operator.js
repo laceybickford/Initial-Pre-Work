674842f5a94065c45b0baa573ee85f276ec363f8
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 15:55:11 -0600

    Completed lesson 5.58

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Learend about strict equality operator. */ 
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(7);
