61cd4a15689fbdf298b21ca254dffb8011e14e62
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 10:21:46 -0600

    Completed lesson 5.27

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+/*LESSON SUMMARY: Constructed a string with a string variable. */
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Lacey Bickford";
+var myStr = "My name is " + myName + " and I am well!";
