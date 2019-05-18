d4cab7518bcd94309913aab869a378d95d668527
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 15:44:09 -0600

    Completed lesson 5.55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+/*LESSON SUMMARY: Learned about Boolean data types. */ 
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
