dde2740ff2a360a9a7e0c75ee8b6e06c47bb191b
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 14:49:52 -0600

    Completed lesson 5.52

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,17 @@
+/* LESSON SUMMARY: Learned about the addSum function. Created a function without any arguments. */
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+var sum = 0;
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
