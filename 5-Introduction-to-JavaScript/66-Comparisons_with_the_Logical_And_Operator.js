b4b91c41c3d0b3eba97d3ea2be778b4f2744e7ab
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 16:13:56 -0600

    Completed lesson 5.66

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,17 @@
+/*LESSON SUMMARY: Used the & operator to compare more than one operator at a time.  */
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+
+      return "Yes";
+
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
