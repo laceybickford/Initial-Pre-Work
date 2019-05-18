a58bcb03c9177426aa9675acc676240c9e51a728
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 15:53:14 -0600

    Completed lesson 5.57

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Learned about the equality operator. */
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
