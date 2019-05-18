917999bd80cd022a134a021b3942d57b222dd701
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 17:46:01 -0600

    Completed lesson 5.80

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+/*LESSON SUMMARY: Built a JavaScript object about my dog. */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+"name": "Lady",
+"legs": 4,
+"tails": 1,
+"friends": ["Idgie", "Peanut"]
+
+
+
+};
