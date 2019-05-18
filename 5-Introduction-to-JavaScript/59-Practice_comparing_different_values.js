d8ca4d47d29b088f5ff825a8eaceed5594b9b63c
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 15:58:08 -0600

    Completed lesson 5.59

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Practiced comparing values to see if they are strictly equal. */ 
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
