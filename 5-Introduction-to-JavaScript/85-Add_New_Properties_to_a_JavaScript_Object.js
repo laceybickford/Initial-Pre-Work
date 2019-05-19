7a913d620060fefc23c000e3f1b61b64d3c85744
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 18:14:00 -0600

    Completed lesson 5.85

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+/*LESSON SUMMARY: Added a new property using brackets. */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog["bark"] = "woof";
