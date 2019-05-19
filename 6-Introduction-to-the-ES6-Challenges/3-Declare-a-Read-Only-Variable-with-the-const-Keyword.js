7219818823289f399faf1b65044f301edae94a26
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 11:45:32 -0600

    Completed lesson 6.3

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,16 @@
+/*LESSON SUMMARY: Declared a read only variable with the const keyword.*/
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
