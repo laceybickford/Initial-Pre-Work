033c3f96cc63cdef57563c48bcf633ae8f4c12a8
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 18:56:07 -0600

    Completed lesson 5.90

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+/*LESSON SUMMARY: Used dot notation and brackets to access nested objects within objects. */
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
