da7d47d1e5976e61b342ce0d281e50f1608204d5
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 18:50:17 -0600

    Completed lesson 5.89

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+/*LESSON SUMMARY: Learned about JSON, a way to store data. */
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Justin Timberlake",
+    "title": "Man of The Woods",
+    "release_year": 2018,
+    "formats": [
+      "CD",
+      "digital",
+      "Vinyl"
+    ],
+    "gold": true
+  }
+];
