afcf1e66e3eb7d41b91c9835fc4d5d05087b65af
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 10:37:42 -0600

    Completed lesson 5.102

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*LESSON SUMMARY: Generated a random whole number using Math.floor & Math.random.  */
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+  return Math.floor(Math.random()*10);
+
+}
