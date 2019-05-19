d98675c8e09c1f9d60d9984a2e24c65cb886c6a4
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 11:41:26 -0600

    Completed lesson 6.2

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Compared scopes of the var and let keywords. */
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
