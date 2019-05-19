b6d9240747d6945b680cd42546e5750f20077fad
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 10:31:57 -0600

    Completed lesson 5.101

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*LESSON SUMMARY: Used Math.random to generate a random number from 0 to just under 1.  */
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
