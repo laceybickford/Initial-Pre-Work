e079585abbdf52a4f900d2cd1eb3966a39454de1
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-18 18:11:58 -0600

    Completed lesson 5.84

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+/*LESSON SUMMARY: Updated an object property using brackets. */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog["name"] = "Happy Coder"
