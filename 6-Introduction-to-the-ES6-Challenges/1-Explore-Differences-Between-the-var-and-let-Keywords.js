19f0026a84ec6ee294a63c701bfb595df1d661ee
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 11:22:12 -0600

    Completed lesson 6.1

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+/*LESSON SUMMARY: Learned to use let keyword for variables that shouldnt be changed. */
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
