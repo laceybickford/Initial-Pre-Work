56721bc6e391323767af482a9dd4077cf395ddcf
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-20 16:55:42 -0600

    Completed lesson 6.20

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,16 @@
+/*LESSON SUMMARY: Learned about using the keyword class. Used class to define a constructor. */
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+  constructor(name){
+    this.name = name
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
