afdbe70b5e1e974a45051f82416092c72f92064c
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-20 16:06:52 -0600

    Completed lesson 6.12

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+/*LESSON SUMMARY: Used destructuring assignment to assign variables from objects. */
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const { tomorrow : tempOfTomorrow } = avgTemperatures;
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
