658385d419224794c48bd81aa161210275312af6
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 22:07:16 -0600

    Completed lesson 6.7

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+/*LESSON SUMMARY: Wrote an arrow fucntion with parameters to pass more than one argument. */
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
