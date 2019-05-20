c9d7c3d7468af631ee668804adc23025197f4f98
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-20 16:27:33 -0600

    Completed lesson 6.15

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,13 @@
+/*LESSON SUMMARY: Used destructuring with the rest operator to reassign array elements. */
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a,b,...arr] = list;
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
