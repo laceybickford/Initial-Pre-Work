7293f39c69ef07dcc183c71070671465d1a17538
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-20 15:57:46 -0600

    Completed lesson 6.11

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+/*LESSON SUMMARY: Used the spread operator to spread arr1 into coma seperated values. */
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
