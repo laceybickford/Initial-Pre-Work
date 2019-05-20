9973de3aee03fd4917062db9d60b88c263ce9cb2
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-19 22:00:01 -0600

    Completed lesson 6.5

---
+++ 6-Introduction-to-the-ES6-Challenges/5-Prevent-Object-Mutation.js
@@ -0,0 +1,19 @@
+/*LESSON SUMMARY: Prevented object mutation by using the fucntion object.freeze. */
+
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
