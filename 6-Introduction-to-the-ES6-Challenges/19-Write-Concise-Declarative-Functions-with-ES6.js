471c051c61fe270e2bc28babd4507ba733e5bc77
Lacey Bickford <mrslaceybickford@gmail.com>
2019-05-20 16:50:24 -0600

    Completed lesson 6.19

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+/*LESSON SUMMARY: Learned that you can remove the function keyword and colon altogether when defining functions in objects.*/
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
